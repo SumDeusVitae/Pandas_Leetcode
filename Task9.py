7623879bb3cf3b73473b0362ad5a3330ffc307a9
Kyrylo Sorokovskyi <Kyrylosorokovskyi@gmail.com>
2024-02-01 23:52:42 -0600

    task 9 added

---
+++ Task9.py
@@ -0,0 +1,12 @@
+# 2885. Rename Columns
+# Write a solution to rename the columns as follows:
+# id to student_id
+# first to first_name
+# last to last_name
+# age to age_in_years
+#
+import pandas as pd
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+    return students.rename(columns={'id':'student_id', 'first':'first_name', 'last':'last_name', 'age':'age_in_years'})
+     
